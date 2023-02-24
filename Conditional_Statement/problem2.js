c5597d371d0d669490c74048ec8a4153332d375c
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-02-24 19:43:51 +0530

    Update problem2.js

--- Conditional_Statement/problem2.js
+++ Conditional_Statement/problem2.js
@@ -1,7 +1,7 @@
-/*1 Augest 2022
-PROBLEM 2
+/*
 Problem 2: If a person is allowed to drive in India print "Apply for a license" or "NA".
 */
+
 let prince_age=14;
 let license_age=18;
 if(license_age<=prince_age){

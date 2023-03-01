351ef41d4bcaf3b6fc69eb083d663d8136c40ac0
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-01 21:35:29 +0530

    Update problem1.js

--- Introduction to JS (Variables-and-Data-Types)/problem1.js
+++ Introduction to JS (Variables-and-Data-Types)/problem1.js
@@ -1,1 +1,10 @@
+/*Problem 1  
+Print "My School" in the console followed by "A Transformation in Education" in next line*/
+
+a ="My School" 
+b ="A Transformation in Education" 
+
+console.log(a);
+
+console.log(b);
 

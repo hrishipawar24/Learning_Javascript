d3f9970b2f3192a9ad2501b25a24cda293e545ad
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-03 19:57:47 +0530

    Update problem3.js

--- Introduction to JS (Variables-and-Data-Types)/problem3.js
+++ Introduction to JS (Variables-and-Data-Types)/problem3.js
@@ -1,1 +1,11 @@
+/*Problem 3 
+1.Create a variable name to store your name
+2.Create a variable age to store your age
+3.Print the values stored in the variable on one line followed by the type of the variable in the next line*/
 
+let name ="Hrishikesh";
+let age  = 22 ;
+
+console.log(name,age);
+
+console.log(typeof(name),typeof(age));

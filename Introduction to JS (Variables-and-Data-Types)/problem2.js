6c967f1dbaf48489327b4f69200e8c6a991832d2
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-03 19:56:35 +0530

    Update problem2.js

--- Introduction to JS (Variables-and-Data-Types)/problem2.js
+++ Introduction to JS (Variables-and-Data-Types)/problem2.js
@@ -1,1 +1,19 @@
+/*Problem 2 
+Define a variable called name with your Name as the assigned value
+Print the value stored in the variable name
+Change the variable to store your father's name
+Print the value stored in the variable name
+Change the variable again to store your mother's name.
+Print the value stored in the variable name*/
 
+let name= "Hrishikesh";
+
+console.log(name);
+
+let father_name= "A. Pawar";
+
+console.log(father_name);
+
+let mother_name= "S. Pawar";
+
+console.log(mother_name);

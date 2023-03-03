8e45151dfd610f126b842cc1cf0822909876103f
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-03 19:59:17 +0530

    Update problem4.js

--- Introduction to JS (Variables-and-Data-Types)/problem4.js
+++ Introduction to JS (Variables-and-Data-Types)/problem4.js
@@ -1,1 +1,30 @@
+/*
+Problem 4
+Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+Print the report card of the student (You can make it look nice by using some keyboard symbols )
+ */
+let h="**************************"
+console.log(h)
+let name= "Student Name::Hrishikesh";
+console.log(name);
+let school= "School Name::Karmvir Vidhyalaya";
+console.log(school);
+let grade = "Grade::A";
+console.log(grade);
+let section ="Section::B2";
+console.log(section);
+let rollno ="Roll no::48";
+console.log(rollno);
+let z="##########********#############"
+console.log(z)
+let name4= "$$ Subject and Marks $$";
+console.log(name4);
+let marks1_physics ="physics =100/85";
+console.log(marks1_physics);
+let marks2_chemistry ="chemistry =100/88";
+console.log(marks2_chemistry);
+let mark3_biology ="biology =100/80";
+console.log(mark3_biology);
+m="##########********#############"
+console.log(m)
 

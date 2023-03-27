dd9af7278709491becabbac3bf6ebcf422ebcba8
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-27 22:07:18 +0530

    Update problem1.js

--- Multi Dimensional Array/problem1.js
+++ Multi Dimensional Array/problem1.js
@@ -1,1 +1,25 @@
-
+/* Problem 1
+Given a square matrix print both the diagonals
+*/
+let arr=[
+  [1,2,3,0],
+  [4,5,6,8],
+  [7,8,9,9],
+  [9,9,9,7]
+];
+let rows=arr.length;
+let cols=arr[0].length;
+let left="";
+let right="";
+for (let i=0;i<=rows-1;i++){
+  for (let j=0;j<=cols-1;j++){
+    if(i==j){
+      left=left+arr[i][j]+" ";
+    }
+    if(i+j==rows-1){
+      right=right+arr[i][j]+" ";
+    }
+  }
+}
+console.log("Left diagonal is", left); //1597
+console.log("Right diagonal is", right); //0689

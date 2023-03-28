81f03ffd8a10ee7395c1c6cde7dc1b4ff34486ad
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-28 16:08:43 +0530

    Update problem1.js

--- Object Method/problem1.js
+++ Object Method/problem1.js
@@ -1,6 +1,5 @@
-/* Problem
+/*Problem -
 Create an object with the following functionality
-
 Ability to add student details and 3 subject marks
 Methods to find the student with the least and highest total
 */

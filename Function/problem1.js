634516fea179ab4b7f61329e5076938d189cdd00
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-30 21:21:35 +0530

    Update problem1.js

--- Function/problem1.js
+++ Function/problem1.js
@@ -2,6 +2,7 @@
 Rectangle Object
 Create a rectangle object with length and width properties
 Create two methods area and perimeter, that will return the area and perimeter of the rectangle*/
+
 let obj={
   length : 20,
   width : 10,

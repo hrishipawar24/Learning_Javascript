9b0ffc7417f6c36153803891f8ea4e05d12791fb
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-30 21:20:54 +0530

    Update problem1.js

--- Function/problem1.js
+++ Function/problem1.js
@@ -1,1 +1,22 @@
+/*
+Problem-1 Rectangle Object
+Create a rectangle object with length and width properties
+Create two methods area and perimeter, that will return the area and perimeter of the rectangle*/
+let obj={
+  length : 20,
+  width : 10,
 
+  area: function(){
+    return (this.length*this.width);
+  },
+
+  peri : function(){
+  return (2*(this.length+this.width));
+  }
+}
+
+let x= obj["area"]();
+let y = obj["peri"]();
+
+console.log("area is: ",x);
+console.log("perimeter is: ",y);

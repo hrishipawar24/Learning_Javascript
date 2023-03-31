2f6e43663e3e2f500972d6e392fb14543e56b8c4
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-03-31 18:40:15 +0530

    Update problem1.js

--- Object/problem1.js
+++ Object/problem1.js
@@ -1,1 +1,17 @@
+/* Problem 1 :
+Given an array find the unique items in the array
+*/
 
+let arr=[1,1,2,2,3,3,4,4,5,6,6,7,7];
+let obj={};
+for(let i=0; i<=arr.length-1; i++){
+  if(obj[arr[i]]==undefined){
+    obj[arr[i]]=1
+  }else{
+    obj[arr[i]]++
+  }
+}
+for(let key in obj){
+  if(obj[key]==1){ //its use for object key
+    console.log(key,"number is unique")
+  }

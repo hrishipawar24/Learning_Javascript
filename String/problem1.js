2199105d58ac894bb35e069aec3bea54ddd59a7a
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-04-02 19:03:07 +0530

    Update problem1.js

--- String/problem1.js
+++ String/problem1.js
@@ -1,1 +1,16 @@
+/*Problem 1 : 
+Given a string count the number of words in that string
+*/
+
+let str="I am best in the world";
+
+let count=0;
+for(let i=0; i<=str.length-1; i++){
+  
+  if(str[i]==" "){
+    count=count+1;
+    
+  }
+ 
+} console.log("Total words:-",count+1)
 

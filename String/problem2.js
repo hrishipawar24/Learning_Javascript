6f0f85207e81f3444a2d0b44b8ee30d90862606e
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-04-02 19:03:56 +0530

    Update problem2.js

--- String/problem2.js
+++ String/problem2.js
@@ -1,1 +1,15 @@
+/*Problem 2 :
+Given a string in lower case convert it to upper case
+*/
+let name="ramakantaacharekar";
+
+let lower="abcdefghijklmnopqrstuvwxyz";
+let upper="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let bag="";
+
+for(let i=0; i<=name.length-1; i++){
+for(let j=0;j<=lower.length-1; j++){
+  if(name[i]==lower[j]){
+      bag=bag+upper[j];
+    } } } console.log(bag)
 

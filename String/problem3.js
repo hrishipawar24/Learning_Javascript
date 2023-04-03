d18de4ad36a433d52f0c855d953fdc0122d5f641
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-04-03 20:13:23 +0530

    Update problem3.js

--- String/problem3.js
+++ String/problem3.js
@@ -1,1 +1,13 @@
+/*
+Problem 3 : Given an array of string count the overall total number of characters
+*/
 
+let arr=["chuunu","mannu","pannu","kunnu"];
+let bag=0;
+let count=0;
+for(let i=0; i<=arr.length-1; i++){
+  
+  bag=bag+arr[i].length;
+  //bag use store i
+}
+console.log("In this string thair are Character:-",bag);

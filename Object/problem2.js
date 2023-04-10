07ae4d92a3499394ae219d86748ee0a3f614d8c6
Hrishikesh Pawar <70955531+hrishipawar24@users.noreply.github.com>
2023-04-10 21:58:30 +0530

    Update problem2.js

--- Object/problem2.js
+++ Object/problem2.js
@@ -1,1 +1,14 @@
+/
+Problem 2 :
+Given a string print, the number of times each character appears
+*/
 
+let arr="hrishikesh";
+let obj={};
+for(let i=0; i<=arr.length-1; i++){
+  if(obj[arr[i]]==undefined){
+    obj[arr[i]]=1
+  }else{
+    obj[arr[i]]++
+  }
+}console.log(obj);
